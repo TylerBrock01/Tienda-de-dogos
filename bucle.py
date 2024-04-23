844dc5f0324663d99113668409444f1900e01bdb
Tyler Brock <pedrohardbass@gmail.com>
2024-04-23 16:13:07 -0700

    Se hizo merge con la branch prueba

--- bucle.py
+++ bucle.py
@@ -1,8 +1,4 @@
 import home
 def bucle_menu(self):
-<<<<<<< HEAD
-    home.funcion_menu()
-=======
     while True: #se modifico esto
         home.funcion_menu()
->>>>>>> f8432f8644ccc806854d954ff5461a0ea8a2bfa0
