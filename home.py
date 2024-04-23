18c61db826d11a885195ca4ad03c4051564dd491
Tyler Brock <pedrohardbass@gmail.com>
2024-04-22 21:04:21 -0700

    se cambio el nombre

---
+++ home.py
@@ -0,0 +1,3 @@
+#invocar menu
+def funcion_menu(self,respuesta):
+    respuesta = input("Bienvenido a dogoshit\nle mostramos el menu\n [1] Ordenar\n [2] Modificar\n [3] verificar pedido\n [0] salir")
